9ae065343121f0b94ae1c15554a14cecad9679f5
medecau <medecau@gmail.com>
2009-09-18 22:03:29 +0100

    rss feed

---
+++ feed.py
@@ -0,0 +1,38 @@
+import cgi
+from google.appengine.ext.webapp import template, RequestHandler, WSGIApplication
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import memcache
+from google.appengine.ext import db
+
+
+class Torrent(db.Model):
+  info_hash = db.StringProperty()
+  content = db.TextProperty()
+  date = db.DateTimeProperty(auto_now_add=True)
+  
+class MainPage(RequestHandler):
+  def get(self, feed_type):
+    feed_cache=memcache.get(feed_type)
+    if feed_cache is None:
+      import os
+      torrents_query = Torrent.all().order('-date')
+      torrents = torrents_query.fetch(25)
+      template_values = {
+        'torrents': torrents,
+        }
+      path = os.path.join(os.path.dirname(__file__), 'rss.xml')
+      feed_cache=template.render(path, template_values)
+
+      memcache.set(feed_type, feed_cache)
+
+      
+    self.response.out.write(feed_cache)
+
+application = WSGIApplication([('/(rss).xml', MainPage)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
